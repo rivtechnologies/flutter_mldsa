53c867d014278c1838eea1301a66c988824a5181
Ammar Elsabe <ayasser763@gmail.com>
2025-06-10 20:09:48 +0400

    ci,fix: last one i swear

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <version>"
     exit 1
@@ -12,10 +14,5 @@ elif git cat-file -t "$tag" | grep -q '^tag$'; then
     echo "  → Annotated tag: $tag"
     annotation=$(git for-each-ref "refs/tags/$tag" --format='%(contents)')
 
-    # insert the heading on line 1
-    sed -i "1i## $tag" CHANGELOG.md
-    # insert a blank line after the heading
-    sed -i $'2i\n' CHANGELOG.md
-    # insert the annotation on line 2
-    sed -i $"3i* $annotation\n" CHANGELOG.md
+    printf "## $tag\n\n* $annotation\n\n" | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
 fi
